562885e196c3c39d1e0279f22ea65d228d6798b1
りょう <143770907+VEDA00133912@users.noreply.github.com>
2025-06-05 05:00:59 +0000

    フォントの拡張子名修正

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ let isFontLoaded = false;
 submitButton.disabled = true;
 downloadButton.disabled = true;
 
-const fontDatas = [["FOT", "./fonts/fot.otf"], ["Kukde", "./fonts/Kukde.otf"], ["Russia", "./fonts/EBG.otf"]];
+const fontDatas = [["FOT", "./fonts/fot.otf"], ["Kukde", "./fonts/Kukde.otf"], ["Russia", "./fonts/EBG.ttf"]];
 Promise.all(fontDatas.map((fontData) => new FontFace(fontData[0], `url('${fontData[1]}')`)
   .load()
   .then((loadedFont) => document.fonts.add(loadedFont))))
